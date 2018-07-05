6e09200e23c2c8957de2b099c5e3a12f9f8447b1
skirka2007 <40521598+skirka2007@users.noreply.github.com>
2018-07-05 18:27:17 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
+gunicorn -b 0.0.0.0:8080 hello:app
