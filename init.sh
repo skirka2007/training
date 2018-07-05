5933b960d7dc1d42420daa97f34e3822d4c04565
skirka2007 <40521598+skirka2007@users.noreply.github.com>
2018-07-05 18:31:09 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 
