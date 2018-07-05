6be07f9c40b159485c1270d11ce3e371b7200e19
skirka2007 <skirka2007@yandex.ru>
2018-07-05 15:42:50 +0300

    2.2

---
+++ etc/hello.py
@@ -0,0 +1,12 @@
+CONFIG = {
+    'mode': 'wsgi',
+    'working_dir': '/home/box/web',
+    'args': (
+        '--bind=0.0.0.0:8000',
+        '--access-logfile access.log',
+        '--error-logfile error.log',
+        '--workers=4',
+        '--timeout=60',
+        'hello:app',
+    ),
+}
