1c9f7b4a10ea35d8a491a2d22642e6215c110f92
skirka2007 <40521598+skirka2007@users.noreply.github.com>
2018-07-05 18:16:40 +0300

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,13 +1,6 @@
-CONFIG = {
-  'mode': 'wsgi',
-  'working_dir': '/home/box/web',
-  'python': '/usr/bin/python3',
-  'args': (
-    '--bind=0.0.0.0:8080',
-    '--workers=2',
-    '--timeout=15',
-    '--log-level=debug',
-    'hello:app'
-  )
-}
+# gunicorn configuration file
+
+pythonpath = '/home/box/web'
+bind = "0.0.0.0:8080"
+workers = 4
 
