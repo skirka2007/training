593b2925fabc3ac96cd0fa274d6cbc7a36a1dfa3
skirka2007 <skirka2007@yandex.ru>
2018-07-05 15:40:19 +0300

    2.1

--- hello.py
+++ hello.py
@@ -3,6 +3,6 @@ def app(environ, start_response):
 	headers = [
 		('Content-Type', 'text/plain')
 	]
-	body = [bytes(i + '/n', 'ascii') for i in environ['QUERY_STRING'].split('&') ]
+	body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
 	start_response(status, headers)
 	return body
