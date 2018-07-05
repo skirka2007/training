3079d1618c9f2151dec53e64d50f613e6da8242a
skirka2007 <40521598+skirka2007@users.noreply.github.com>
2018-07-05 18:15:24 +0300

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,12 +1,13 @@
 CONFIG = {
-    'mode': 'wsgi',
-    'working_dir': '/home/box/web',
-    'args': (
-        '--bind=0.0.0.0:8000',
-        '--access-logfile access.log',
-        '--error-logfile error.log',
-        '--workers=4',
-        '--timeout=60',
-        'hello'
-    )
+  'mode': 'wsgi',
+  'working_dir': '/home/box/web',
+  'python': '/usr/bin/python3',
+  'args': (
+    '--bind=0.0.0.0:8080',
+    '--workers=2',
+    '--timeout=15',
+    '--log-level=debug',
+    'hello:app'
+  )
 }
+
