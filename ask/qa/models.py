67f2f9672a7e816714085acdb513980334a15ed9
skirka2007 <skirka2007@yandex.ru>
2018-07-17 15:45:30 +0300

    4.4

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -20,7 +20,7 @@ class Question(models.Model):
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateField(blank = True, auto_now_add=True)
-    question = models.ForeignKey(Question, on_delete=models.SET_NULL)
-    author = models.ManyToManyField(User, on_delete=models.SET_NULL)
+    question = models.ForeignKey(Question)
+    author = models.ManyToManyField(User)
 
 
